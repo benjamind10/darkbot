ab006ccdbfd2f2c2cfe05ec3dbbbd2ab9d5469cc
benjamind10 <ben@bytebug.io>
2024-05-23 20:08:48 +0000

    added changes to logs

--- bot/logging_files/moderation_logging.py
+++ bot/logging_files/moderation_logging.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 
 logger = logging.getLogger(__name__)
@@ -6,7 +7,9 @@ logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
 
-file_handler = logging.FileHandler("./logs/moderation.log", maxBytes=10*1024*1024, backupCount=1)
+file_handler = RotatingFileHandler(
+    "./logs/moderation.log", maxBytes=10 * 1024 * 1024, backupCount=1
+)
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
